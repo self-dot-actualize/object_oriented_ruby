b3c157604ee20114b9c9bfe68212915cb704ae91
Peter Jang <peter.x.jang@gmail.com>
2017-02-28 18:39:42 -0600

    Add solution to bike/car exercise

--- manager.rb
+++ manager.rb
@@ -30,25 +30,7 @@ employee2.active = false
 puts employee2.active
 puts
 
-class Manager
-  attr_reader :first_name, :last_name, :active
-  attr_writer :active
-
-  def initialize(input_options)
-    @first_name = input_options[:first_name]
-    @last_name = input_options[:last_name]
-    @salary = input_options[:salary]
-    @active = input_options[:active]
-  end
-
-  def print_info
-    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
-  end
-
-  def give_annual_raise
-    @salary = 1.05 * @salary
-  end
-
+class Manager < Employee
   def send_report
     puts "Sending email..."
     # use email sending library
