04ffe7befdcbef7966d4c400fea886328058908e
Peter Jang <peter.x.jang@gmail.com>
2017-03-01 16:53:53 -0600

    Add different attributes to Bike and Car classes

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,5 +1,5 @@
 class Vehicle
-  def initialize
+  def initialize(input_options)
     @speed = 0
     @direction = 'north'
   end
@@ -18,18 +18,39 @@ class Vehicle
 end
 
 class Car < Vehicle
+  def initialize(input_options)
+    super
+    @make = input_options[:make]
+    @model = input_options[:model]
+    @year = input_options[:year]
+    @fuel = input_options[:fuel]
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Vehicle
+  def initialize(input_options)
+    super
+    @gears = input_options[:gears]
+    @type = input_options[:type]
+    @weight = input_options[:weight]
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-car = Car.new
+car = Car.new(make: "Ford", model: "Explorer", year: 2000, fuel: 19)
 car.honk_horn
-bike = Bike.new
+bike = Bike.new(gears: 10, type: "Mountain", weight: 20)
 bike.ring_bell
+bike.accelerate
+p bike
+
+vehicle = Vehicle.new({})
+vehicle.accelerate
+p vehicle
