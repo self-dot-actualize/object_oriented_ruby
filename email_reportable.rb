3b6f6b31f4ca7db7c5830df7656839f99d1325e2
Peter Jang <peter.x.jang@gmail.com>
2017-03-01 19:19:11 -0600

    Refactor classes and modules into separate files

---
+++ email_reportable.rb
@@ -0,0 +1,13 @@
+module EmailReportable
+  def send_report
+    puts "Sending email..."
+    # use email sending library
+    puts "Email sent!"
+  end
+
+  def receive_report
+    puts "Receiving report..."
+    # write some code
+    puts "Email received!"
+  end
+end
---
+++ main.rb
@@ -0,0 +1,4 @@
+require "./employee.rb"
+
+employee1 = Employee.new(first_name: "Majora", last_name: "Carter", salary: 80000, active: true)
+employee1.print_info
