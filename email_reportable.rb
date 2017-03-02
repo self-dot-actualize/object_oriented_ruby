78e83b079c2707648be8f66d4cc640a97402d753
Peter Jang <peter.x.jang@gmail.com>
2017-03-01 19:45:35 -0600

    Use module to namespace all classes and modules

--- email_reportable.rb
+++ email_reportable.rb
@@ -1,13 +1,15 @@
-module EmailReportable
-  def send_report
-    puts "Sending email..."
-    # use email sending library
-    puts "Email sent!"
-  end
+module Actualize
+  module EmailReportable
+    def send_report
+      puts "Sending email..."
+      # use email sending library
+      puts "Email sent!"
+    end
 
-  def receive_report
-    puts "Receiving report..."
-    # write some code
-    puts "Email received!"
+    def receive_report
+      puts "Receiving report..."
+      # write some code
+      puts "Email received!"
+    end
   end
 end
